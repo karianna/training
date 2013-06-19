06c5ccab0a31b406ca31fa11bb3b0ce7d63e92c3
Martijn Verburg <martijnverburg@gmail.com>
2013-06-19 16:02:38 +0100

    Fixes for JMS tests

--- advanced_java/src/main/java/org/jclarity/training/chapter11/JmsMessageListener.java
+++ advanced_java/src/main/java/org/jclarity/training/chapter11/JmsMessageListener.java
@@ -10,6 +10,7 @@ import javax.jms.TextMessage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jms.listener.DefaultMessageListenerContainer;
 import org.springframework.stereotype.Component;
 
 @Component
